66a1e505d43a946b98c57372133e915d586e9388
switefaster <2734825077@qq.com>
2018-02-03 21:34:15 +0800

    Added "function":"bossbars"

---
+++ src/command-node/functions/bossbars.ts
@@ -0,0 +1,19 @@
+/*
+ * Bossbars node
+ * 
+*/
+
+import BaseNode from './../base'
+import {getResources} from './../../resources'
+import {indexOf} from './../../util'
+
+export default class BossbarNode extends BaseNode {
+    getCompletion (line: string, start: number, end: number, data): [Array<string>, boolean] {
+        let index = indexOf(line, start, end, ' ');
+        if(index !== -1) {
+            return super.getCompletion(line, index+1, end, data);
+        }
+        let segment = line.substring(start, end);
+        return [getResources("bossbars").filter(v=>v.startsWith(segment)), true];
+    }
+}
