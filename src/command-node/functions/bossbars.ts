43590db4791dd04937431b214ded57e533f59079
switefaster <2734825077@qq.com>
2018-02-04 12:21:39 +0800

    Added resource component completion

--- src/command-node/functions/bossbars.ts
+++ src/command-node/functions/bossbars.ts
@@ -5,7 +5,7 @@
 
 import BaseNode from './../base'
 import {getResources} from './../../resources'
-import {indexOf} from './../../util'
+import {indexOf, getResourceComponents} from './../../util'
 
 export default class BossbarNode extends BaseNode {
     getCompletion (line: string, start: number, end: number, data): [Array<string>, boolean] {
@@ -14,6 +14,26 @@ export default class BossbarNode extends BaseNode {
             return super.getCompletion(line, index+1, end, data);
         }
         let segment = line.substring(start, end);
-        return [getResources("bossbars").filter(v=>v.startsWith(segment)), true];
+        return [bossbarCompletion(line, start, end), true];
     }
+}
+
+export function bossbarCompletion(line: string, start: number, end: number): Array<string> {
+    let components = getResourceComponents(line.substring(start, end));
+    let temp = getResources("bossbars");
+    if(components.length === 2 && indexOf(line, start, end, ':') === -1) {
+        let children = Object.keys(temp);
+        temp = temp["minecraft"] || [];
+        children.push(...temp);
+        return children;
+    }
+    let children = [];
+    for(let i = 0; i < components.length-1; i++) {
+        if(temp[components[i]]) {
+            children.push(...(temp[components[i]]));
+        } else {
+            return [];
+        }
+    }
+    return children;
 }
